4751a91a97fdcb718026559e57b83fbdb14b4bd4
Julio Moreno <141185519+Dokh0@users.noreply.github.com>
2025-06-02 13:05:32 +0100

    Step 2 gradeBookApp.js

--- gradeBookApp.js
+++ gradeBookApp.js
@@ -11,3 +11,28 @@ function getAverage(scores) {
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+//STEP 2
+function getGrade(score) {
+  let grade
+  if (score === 100) {
+    grade = "A++";
+  } else if (score >= 90 && score <= 99) {
+    grade = "A";
+  } else if (score >= 80 && score <= 89) {
+    grade = "B";
+  } else if (score >= 70 && score <= 79) {
+    grade = "C";
+  } else if (score >= 60 && score <= 69) {
+    grade = "D";
+  } else {
+    grade = "F"
+  }
+  return grade
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
+
+//STEP 3
